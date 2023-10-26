5c4ba5d466ed01a63746f84e9e217d9102030f4a
fan <xiaofanms@outlook.com>
2023-10-26 17:47:22 +0800

    错误提示更改为modal展示。

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -1,22 +1,24 @@
 import {inject, Injectable} from '@angular/core';
-import {NzMessageService} from "ng-zorro-antd/message";
 import {errorMessage, ResultError} from "../util";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {Router} from "@angular/router";
+import {NzModalService} from "ng-zorro-antd/modal";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
 
-  private message = inject(NzMessageService)
+  private modal = inject(NzModalService)
 
   private router = inject(Router)
 
   process(err: any, topic?: string) {
     console.debug(err);
     const msg = errorMessage(err, topic);
-    this.message.error(msg);
+    this.modal.error({
+      nzTitle: '操作失败',
+      nzContent: msg
+    });
     if (err instanceof ResultError && "1" === err.code) {
       this.router.navigateByUrl("/login");
     }
