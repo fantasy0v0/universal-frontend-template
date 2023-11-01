bc921892d9d6b8463a3438848e0e6aa379a9f225
fan <xiaofanms@outlook.com>
2023-11-01 10:11:36 +0800

    调整全局进度条的体验。

--- src/ng-progress/ng-progress-http.interceptor.ts
+++ src/ng-progress/ng-progress-http.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {debounceTime, Observable, Subject} from 'rxjs';
 import { NgProgress } from './ng-progress.service';
 import { finalize } from 'rxjs/operators';
 
@@ -13,18 +13,25 @@ export class NgProgressHttpInterceptor implements HttpInterceptor {
 
   private _inProgressCount = 0;
 
-  constructor(private nProgress: NgProgress) { }
+  private subject = new Subject<void>();
+
+  constructor(private nProgress: NgProgress) {
+    this.subject.asObservable()
+      .pipe(debounceTime(300))
+      .subscribe(() => {
+        if (this._inProgressCount === 0) {
+          this.nProgress.done();
+        }
+      });
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     this._inProgressCount++;
-
     this.nProgress.start();
     return next.handle(req).pipe(finalize(() => {
+      this.nProgress.inc(.3 + .5 * Math.random());
       this._inProgressCount--;
-      if (this._inProgressCount === 0) {
-        this.nProgress.done();
-      }
+      this.subject.next();
     }));
   }
 }
